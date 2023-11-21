78d0dac7b0d7e397bbe288eee56c51f33048e32c
knottwill <81773569+knottwill@users.noreply.github.com>
2023-11-21 16:56:25 +0000

    Moved code from part e into solve_part_e.py, seems to work.

--- src/solve_part_e.py
+++ src/solve_part_e.py
@@ -1,50 +1,108 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from iminuit import Minuit
 from iminuit.cost import UnbinnedNLL
-from scipy.stats import norm, expon
 
-# import function to generate data
+# True parameter values
+true_params = {'f': 0.1, 'lam': 0.5, 'mu': 5.28, 'sigma': 0.018}
+
+# Upper & Lower bound of M
+alpha = 5
+beta = 5.6
+
+# ---------------
+# Generating data
+# ---------------
 from generation import generate_from_total_pdf
 
-# generate 100000 events
 N_events= 100000
-dset = generate_from_total_pdf(N_events, f=0.1, lam=0.5, mu=5.28, sigma=0.018, alpha=5, beta=5.6)
+dataset = generate_from_total_pdf(N_events, **true_params)
 
-###############################################
-# The models imported from pdfs.py were causing errors
-# I redefine the total_pdf as a temporary solution
-# GOTTA GO BACK AND CHANGE THIS
-###############################################
-def model(M, f, lam, mu, sigma):
-    total_prob = norm.cdf(x=5.6, loc=mu, scale=sigma) - norm.cdf(x=5, loc=mu, scale=sigma)
-    signal = f*norm.pdf(x=M, loc=mu, scale=sigma)/total_prob
+# ---------------
+# Starting parameters for minimisation
+# Our starting parameters are the true parameters + some approprate random shift
+# The shift is limited such that we don't start with 'unphysical' parameters
+# or with parameters too far away from the true parameters
+# ---------------
 
-    total_prob = expon.cdf(x=5.6, scale=1/lam) - expon.cdf(x=5, scale=1/lam)
-    background = (1 - f)*expon.pdf(x=M, scale=1/lam)/total_prob
+random_shifts = {
+    'f': np.random.uniform(-0.09, 0.5), 
+    'lam': np.random.uniform(-0.3, 1), 
+    'mu': np.random.uniform(-0.25, 0.3), 
+    'sigma': np.random.uniform(-0.01, 0.03)
+}
 
-    return signal + background
+# Creating starting parameters
+starting_params = {}
+for param in true_params:
 
-# (unbinned) negative log likelihood as the cost function
-nll = UnbinnedNLL(dset , model)
+    # true parameter + random shift
+    starting_params[param] = true_params[param] + random_shifts[param]
+
+    # round to 4 significant figures
+    starting_params[param] = float(f'{starting_params[param]:.4}')
+
+# ---------------
+# Minimisation Object
+# ---------------
+
+# import our total PDF
+from distributions import total_pdf
+
+# Cost function is negative log likelihood (with factor 2 so it is chi2 distributed)
+nll = UnbinnedNLL(dataset, total_pdf)
 
 # Minimisation object
-# Passing random starting values for the parameters
-alpha, beta = 5, 5.6
-mi = Minuit(
-    fcn = nll,
-    f = np.random.uniform(0,1),
-    lam = np.random.uniform(0,2),
-    mu = np.random.uniform(alpha, beta),
-    sigma = np.random.uniform(0.1,0.7),
-)
+mi = Minuit(nll, **starting_params)
 
-# Minimise the cost function
-mi.migrad()
+# ---------------
+# Setting constraints to parameters 
+# some are physical constaints (eg. sigma > 0) 
+# some are just sensible constraints
+# ---------------
+mi.limits['f'] = (0, 1) # fraction of signal is between 0 and 1
+mi.limits['lam'] = (0, None) # lambda cannot be negative (otherwise there is no 'decay')
+mi.limits['sigma'] = (0, (beta-alpha)/2) # sigma should not be too wide, and cannot be negative
+mi.limits['mu'] = (alpha, beta) # the signal should not peak outside of [alpha, beta]
 
-# Hesse algorithm
-mi.hesse()
+# ---------------
+# Running Minimisation and the error finding algorithms
+# ---------------
 
-# print the fit result
+mi.migrad() # minimisation
+mi.hesse() # finds symmetric uncertainty
+mi.minos() # finds non-symmetric confidence interval
+
 print(mi)
 
+# ---------------
+# Binning and Plotting
+# ---------------
 
+# Bin the events
+bins = 120
+bin_density, bin_edges = np.histogram(dataset, bins=bins, density=True)
+counts, _ = np.histogram(dataset, bins=bins, density=False)
+
+# Calculating uncertainty on bin_density
+count_uncertainties = np.sqrt(counts) 
+bin_widths = np.diff(bin_edges)
+bin_err = count_uncertainties / (len(dataset) * bin_widths)
+
+# Calculate bin midpoints
+midpoints = 0.5 * (bin_edges[:-1] + bin_edges[1:])
+
+fig, ax = plt.subplots()
+
+# Plotting true model, fitting model and the binned data
+ax.errorbar(midpoints, bin_density, yerr=bin_err, fmt='.', capsize=1, label='Binned Data')
+ax.plot(midpoints, total_pdf(midpoints, **true_params), label='True model', color='red')
+ax.plot(midpoints, total_pdf(midpoints, *mi.values), label='Fitted model', color='orange')
+
+ax.set_xlabel('M')
+ax.set_ylabel('Density')
+ax.set_title(f'{N_events} total events')
+
+ax.legend()
+
+fig.savefig('plots/part_e.png')
