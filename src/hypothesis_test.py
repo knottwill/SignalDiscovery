e14ebab18b1684f18e9f48b02ea7f61edb47cb47
knottwill <81773569+knottwill@users.noreply.github.com>
2023-11-22 12:18:12 +0000

    Added functionality to the hypothesis test module to allow for unbinned fits with iminuit. We get very significantly different results which is strange.
    With binned fitting: N_90 ~ 62000
    With unbinned fitting: N_90 ~ 800

--- src/hypothesis_test.py
+++ src/hypothesis_test.py
@@ -2,10 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import chi2, norm
 from iminuit import Minuit
-from iminuit.cost import BinnedNLL
+from iminuit.cost import UnbinnedNLL, BinnedNLL
 from pytest import approx 
 
-def signal_background_test(dataset, cdf, starting_params: dict, print_fitting_results=False, return_variables_for_plotting=False):
+def neyman_pearson_test(h0_nll, h1_nll):
+    # ---------------------------
+    # Perform Neyman-Pearson Test
+    # ---------------------------
+
+    T = h0_nll - h1_nll # test statistic
+    k = 1               # degrees of freedom
+
+    # Calculate p value
+    p_value = 1 - chi2.cdf(T, k) 
+
+    # Calculate significance, only if the p value is larger than 0.5 
+    # (otherwise the significance is meaningless)
+    # (it is a one-sided test)
+    Z = norm.ppf(1 - p_value)
+
+    # Alternate way to calculate Z for a one-sided test
+    # uses the chi2 percentage point function
+    # we assert that the significances are the same 
+    # (except if the p value is greater than 0.5, in which case 1 - 2*p_value evaluates to 
+    # a negative probability, which doesn't make sense)
+    if p_value < 0.5:
+        alternate_Z = np.sqrt(chi2.ppf(1 - 2*p_value,1))
+        assert(Z == approx(alternate_Z))
+
+    return Z, p_value
+
+def signal_background_test(dataset, pdf, cdf, starting_params: dict, binned=False, print_fitting_results=False, return_variables_for_plotting=False):
     """
     Perform Neyman-Pearson Hypothesis test for the existance of a signal
 
@@ -18,6 +45,10 @@ def signal_background_test(dataset, cdf, starting_params: dict, print_fitting_re
     alpha = 5
     beta = 5.6
 
+    # --------------------
+    # Binning
+    # --------------------
+
     # square root rule for number of bins
     bins = int(np.sqrt(len(dataset)))
     
@@ -27,15 +58,21 @@ def signal_background_test(dataset, cdf, starting_params: dict, print_fitting_re
     # Calculate bin midpoints
     midpoints = 0.5 * (bin_edges[:-1] + bin_edges[1:])
 
-    # --------------------
+     # --------------------
     # Minimisation object
     # --------------------
 
-    # Cost function is binned negative log likelihood
-    binned_nll = BinnedNLL(bin_density, bin_edges, cdf)
+    if binned:
+        # Cost function is binned negative log likelihood
+        nll = BinnedNLL(bin_density, bin_edges, cdf)
 
-    # Minimisation object
-    mi = Minuit(binned_nll, **starting_params)
+        # Minimisation object
+        mi = Minuit(nll, **starting_params)
+
+    else:
+        nll = UnbinnedNLL(dataset, pdf)
+        mi = Minuit(nll, **starting_params)
+        
 
     # Setting constraints
     mi.limits['f'] = (0, 1) # fraction of signal is between 0 and 1
@@ -54,9 +91,7 @@ def signal_background_test(dataset, cdf, starting_params: dict, print_fitting_re
     # If valid minimum is not found, print message
     h1_valid = mi.valid
     if not(h1_valid):
-        print('#---------------')
         print('Warning: valid minimum NOT FOUND for H1')
-        print('#---------------')
         print(mi)
         return 'invalid minimum', 0, 0 # don't continue with test
 
@@ -86,9 +121,7 @@ def signal_background_test(dataset, cdf, starting_params: dict, print_fitting_re
     # If valid minimum is not found, print message
     h0_valid = mi.valid
     if not(h0_valid):
-        print('#---------------')
         print('Warning: valid minimum NOT FOUND for H0')
-        print('#---------------')
         print(mi)
         return 'invalid minimum', 0, 0 # don't continue with test
 
@@ -105,25 +138,7 @@ def signal_background_test(dataset, cdf, starting_params: dict, print_fitting_re
     # Perform Neyman-Pearson Test
     # ---------------------------
 
-    T = h0_nll - h1_nll # test statistic
-    k = 1               # degrees of freedom
-
-    # Calculate p value
-    p_value = 1 - chi2.cdf(T, k) 
-
-    # Calculate significance, only if the p value is larger than 0.5 
-    # (otherwise the significance is meaningless)
-    # (it is a one-sided test)
-    Z = norm.ppf(1 - p_value)
-
-    # Alternate way to calculate Z for a one-sided test
-    # uses the chi2 percentage point function
-    # we assert that the significances are the same 
-    # (except if the p value is greater than 0.5, in which case 1 - 2*p_value evaluates to 
-    # a negative probability, which doesn't make sense)
-    if p_value < 0.5:
-        alternate_Z = np.sqrt(chi2.ppf(1 - 2*p_value,1))
-        assert(Z == approx(alternate_Z))
+    Z, p_value = neyman_pearson_test(h0_nll, h1_nll)
 
     # If we get a significance greater than 5, we have 'discovered' the signal
     if Z >= 5:
