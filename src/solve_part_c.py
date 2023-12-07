09e6c36d4e4e001895b6e3179866a45f61f6c3d2
knottwill <81773569+knottwill@users.noreply.github.com>
2023-12-07 10:15:17 +0000

    Finalised part c

--- src/solve_part_c.py
+++ src/solve_part_c.py
@@ -2,16 +2,17 @@ import numpy as np
 from scipy.integrate import quad
 from pytest import approx
 
-# Import the total pdf
+# Import the total PDF
 from distributions import total_pdf
 
 # Define upper and lower bounds
 alpha = 5
 beta = 5.6
 
-# We perform the integration for 1000 random combinations of parameters
+# Integrate the PDF for 1000 random combinations of parameters
 integrates_to_unity = True
 N_combos = 1000
+np.random.seed(42)
 print(f'Integrating PDF over [{alpha}, {beta}] for {N_combos} random combinations of parameters...\n')
 for _ in range(N_combos):
 
@@ -21,22 +22,18 @@ for _ in range(N_combos):
     mu = np.random.uniform(alpha, beta)
     sigma = np.random.uniform(0.1,0.7)
 
-    # Performs integration of total PDF over [5,5.6] given the generated parameters
+    # Performs integration of total PDF over range [alpha, beta]
     total_probability, error = quad(total_pdf, alpha, beta, args=(f, lam, mu, sigma))
 
-    # testing whether the total_probability is approximately 1
-    # with a relative tolerance of 1e-6 (using pytest.approx)
+    # If the PDF did not integrate to 1 (with a relative tolerance of 1e-6) 
+    # then we set integrates_to_unity to False
     relative_tolerance = 1e-6
     if total_probability != approx(1, rel=relative_tolerance):
-
-        print(f'Params: f = {f:.4}, lambda = {lam:.4}, mu = {mu:.4}, sigma = {sigma:.4}')
-        print('Total Probability: {total_probability}')
-
         integrates_to_unity=False # failed
 
 # Print statements if test was succeeded vs failed
 if integrates_to_unity:
-    print(f'As expected, the total PDF integrated to (approximately) unity on all')
-    print(f'{N_combos} trials (with a relative tolerance of {relative_tolerance})\n')
+    print(f'As expected, the total PDF integrated to approximately unity (with a relative ')
+    print(f'tolerance of {relative_tolerance}) on all {N_combos} trials\n')
 else:
     print('Failed.')
