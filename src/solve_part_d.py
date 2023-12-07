0fe029be101585d6b56a46a90b2cda7c26a0cb5d
knottwill <81773569+knottwill@users.noreply.github.com>
2023-12-07 10:32:04 +0000

    Finalised part d

--- src/solve_part_d.py
+++ src/solve_part_d.py
@@ -1,5 +1,19 @@
+"""
+In this file we solve part d. We plot the signal, background and total 
+distributions with the true parameter values in two ways. In the first 
+plot we include the weightings on signal and background such that only 
+the total PDF is properly normalised but it is visually clear that the 
+combination of the signal-only and background-only models makes up the 
+total PDF. In the second plot, we properly normalise all distributions, 
+so that we are seeing the 'True PDFs' of the signal-only, background-only 
+and total PDFs.
+"""
+
 import matplotlib.pyplot as plt
+import matplotlib.style as mplstyle
 import numpy as np
+
+mplstyle.use('src/mphil.mplstyle')
 
 # import pdfs
 from distributions import signal_pdf, background_pdf, total_pdf
@@ -14,58 +28,57 @@ lam = 0.5
 mu = 5.28  
 sigma = 0.018 
 
-assert(alpha > 0)
-assert(beta > 0)
-assert(beta > alpha)
-assert(sigma > 0)
-assert(lam > 0)
-assert(f >= 0 and f <= 1)
-
 # Generate values of M in [alpha, beta]
 M_values = np.linspace(alpha, beta, 1000)
 
-# Evaluate pdfs at all M_values
+# Evaluate normalised PDFs at all M_values
 pdf_signal = signal_pdf(M_values, mu, sigma)
 pdf_background = background_pdf(M_values, lam)
 pdf_total = total_pdf(M_values, f, lam, mu, sigma)
 
 # Add weightings to signal and background
-signal_only = f*pdf_signal
-background_only = (1-f)*pdf_background
+weighted_signal = f*pdf_signal
+weighted_background = (1-f)*pdf_background
 
 # Ensure the total pdf is equal to signal + background (with weightings)
-assert( np.all(pdf_total == signal_only + background_only) )
+assert np.all(pdf_total == weighted_signal + weighted_background) 
 
 
-####################
-## Plotting the PDFs 
-#####################
-fig, axs = plt.subplots(2, 1, figsize=(10, 12))  # Creates a figure with two subplots
+# ---------------------
+# Plotting the weighted PDFs 
+# ---------------------
+fig, ax = plt.subplots(1, 1, figsize=(10, 6)) 
 
 # First Plot - signal and background have weights applied
-# such that we can visually see: total = signal + background
-axs[0].plot(M_values, pdf_total, label='Total Probability Distribution')
-axs[0].plot(M_values, signal_only, label=f'Signal Only (with {f} weighting)', linestyle=':')
-axs[0].plot(M_values, background_only, label=f'Background Only (with {1-f} weighting)', linestyle='--')
-
-axs[0].set_xlabel('M')
-axs[0].set_ylabel('Probability Density')
-axs[0].set_title('Comparison of Probability Distributions (Weighted)')
-axs[0].legend()
-axs[0].grid(True)
-
-# Second Plot - signal and background are normalised PDFs
-# such that all distributions are properly normalised PDFs (integrate to unity)
-axs[1].plot(M_values, pdf_total, label='Total Probability Distribution')
-axs[1].plot(M_values, pdf_signal, label='Signal PDF (Normalized)', linestyle=':')
-axs[1].plot(M_values, pdf_background, label='Background PDF (Normalized)', linestyle='--')
-
-axs[1].set_xlabel('M')
-axs[1].set_ylabel('Probability Density')
-axs[1].set_title('Comparison of Properly Normalized PDFs')
-axs[1].legend()
-axs[1].grid(True)
+# such that we can visually see that total = signal + background
+ax.plot(M_values, pdf_total, label='Total Probability Distribution')
+ax.plot(M_values, weighted_signal, label=f'Signal Only (with {f} weighting)', linestyle=':')
+ax.plot(M_values, weighted_background, label=f'Background Only (with {1-f} weighting)', linestyle='--')
+ax.set_xlabel('M')
+ax.set_ylabel('Probability Density')
+ax.set_title('Probability Distributions (Weighted)')
+ax.legend()
+ax.grid(True)
 
 plt.tight_layout()  # Adjusts the plots to fit into the figure neatly
+fig.savefig(f'plots/part_d_weighted.png')
 
-fig.savefig(f'plots/part_d.png')
+# ---------------------
+# Plotting the weighted PDFs 
+# ---------------------
+
+fig, ax = plt.subplots(1, 1, figsize=(10, 6)) 
+
+# Second Plot - signal and background are normalised PDFs
+# such that all distributions are properly normalised PDFs and integrate to unity
+ax.plot(M_values, pdf_total, label='Total Probability Distribution')
+ax.plot(M_values, pdf_signal, label='Signal PDF (Normalized)', linestyle=':')
+ax.plot(M_values, pdf_background, label='Background PDF (Normalized)', linestyle='--')
+ax.set_xlabel('M')
+ax.set_ylabel('Probability Density')
+ax.set_title('Probability Distributions (Properly Normalized)')
+ax.legend()
+ax.grid(True)
+
+plt.tight_layout()  # Adjusts the plots to fit into the figure neatly
+fig.savefig(f'plots/part_d_normalised.png')
